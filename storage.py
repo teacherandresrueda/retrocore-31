2c3de707f9b9c1d92241df3bfdb44e53aa698383
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-06 10:15:53 -0600

    Refactor data handling to use historial.json

--- storage.py
+++ storage.py
@@ -1,27 +1,16 @@
 import json
 import os
 
-DATA_FILE = "data_users.json"
+FILE = "historial.json"
 
-def cargar_data():
-    if not os.path.exists(DATA_FILE):
-        return {}
-    with open(DATA_FILE, "r") as f:
+def cargar_historial():
+    if not os.path.exists(FILE):
+        return {"jugadas": []}
+    with open(FILE, "r") as f:
         return json.load(f)
 
-def guardar_data(data):
-    with open(DATA_FILE, "w") as f:
+def guardar_jugada(numeros):
+    data = cargar_historial()
+    data["jugadas"].append(numeros)
+    with open(FILE, "w") as f:
         json.dump(data, f)
-
-def guardar_jugada(usuario, jugadas):
-    data = cargar_data()
-
-    if usuario not in data:
-        data[usuario] = []
-
-    data[usuario].append(jugadas)
-    guardar_data(data)
-
-def obtener_historial(usuario):
-    data = cargar_data()
-    return data.get(usuario, [])
